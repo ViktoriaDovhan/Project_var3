f0c94bb5055a6c51f048affa1e21e68320cf5783
Вікторія <v.dovhan@ukma.edu.ua>
2024-03-22 16:26:49 +0200

    Test

--- asm/pr7.asm
+++ asm/pr7.asm
@@ -2,50 +2,70 @@
 .stack 100h
 
 .data
-    buffer db 80h dup(?) ; Оголошення буфера для зберігання даних
+mes db "Success $"
+mesBad db "File error $"
+buffInd db 0; Index to keep track of the current position in buffer
+oneChar db 0
+
+keys db 5000*16 dup(0)
+keyInd dw 0
+isWord db 1
+values db 5000*16 dup(0)
+valInd dw 0
 
 .code
 main proc
-    mov ax, @data  ; Завантаження адреси сегмента даних
-    mov ds, ax     ; Установка DS для доступу до даних
+    mov ax, @data
+    mov ds, ax
 
-    mov ah, 3Fh    ; Функція DOS для читання з файлу
-    mov bx, 0      ; Handle stdin (стандартний ввід)
-    lea dx, buffer ; Вказівник на буфер, куди будуть зчитуватися дані
-    mov cx, 80h    ; Читати 128 байтів (максимальна довжина рядка у DOS)
-    int 21h        ; Виклик DOS-інтерапту для зчитування з файлу
+    mov ah, 0Ah ; Read from standard input (keyboard)
+    mov dx, offset oneChar ; Buffer to store the input
+    int 21h
 
-    cmp ax, cx     ; Порівняння кількості зчитаних байтів з максимальною довжиною рядка
-    jae check_eof  ; Якщо прочитано стільки ж байтів або більше, перевіряємо на кінець файлу
-    mov cx, ax     ; Якщо прочитано менше, корегуємо CX для виведення
+    mov si, offset oneChar ; Set SI to the beginning of the input buffer
+    mov cx, 16 ; Set the counter to the size of the input buffer
+    xor bx, bx ; Clear BX
 
-display:
-    mov ah, 02h    ; Функція DOS для виведення символу
-    mov si, 0      ; Встановлення SI на початок буфера
+readLoop:
+    mov al, [si] ; Move a character from the buffer into AL
+    cmp al, 0Dh ; Check if the character is a carriage return
+    je endRead ; If it is, end the read process
+    mov [keys + bx], al ; Move the character into the keys array
+    inc bx ; Increment the index for the keys array
+    inc si ; Move to the next character in the buffer
+    loop readLoop ; Repeat until all characters are read or buffer is exhausted
 
-print_loop:
-    mov dl, buffer[si] ; Завантаження символу для виведення
-    int 21h            ; Вивід символу
-    inc si             ; Перехід до наступного символу
-    loop print_loop    ; Повторюємо, поки CX не дорівнює 0
+endRead:
+    mov keyInd, bx ; Store the count of characters read into keyInd
 
-check_eof:
-    ; Перевірка на кінець файлу
-    mov ah, 3Eh   ; Функція DOS для перевірки EOF
-    mov bx, 0     ; Handle stdin (стандартний ввід)
-    int 21h       ; Виклик DOS-інтерапту для перевірки EOF
+    ; Process the characters in keys array
+    mov cx, bx ; Set CX to the number of characters read
+    mov si, offset keys ; Set SI to the beginning of the keys array
+processLoop:
+    mov al, [si] ; Load a character from the keys array
+    ; Implement your character processing logic here
+    inc si ; Move to the next character in the keys array
+    loop processLoop ; Repeat until all characters are processed
 
-    ; Якщо вказівник EOF не дорівнює 128 (0x80), це означає, що файл закінчився
-    cmp ax, 80h
-    jne end_program  ; Якщо досягнуто кінець файлу, завершуємо програму
+    ; Output sorted values array
+    mov  si, offset values      ; Set SI to the beginning of the values array
+    mov  cx, valInd            ; Number of elements in the array
 
-    ; Якщо не досягнуто кінець файлу, читаємо далі
-    jmp main
+outputLoop:
+    mov  dl, [si]               ; Load value
+    mov  ah, 02h                ; DOS function for displaying character
+    int  21h                    ; Display character
 
-end_program:
-    ; Завершення програми і повернення до DOS
+    inc  si                     ; Move to the next value
+    loop outputLoop             ; Repeat until all values are printed
+
+    mov ah, 09h
+    mov dx, offset mes
+    int 21h
+
     mov ax, 4C00h
     int 21h
 
 main endp
+
 end main
